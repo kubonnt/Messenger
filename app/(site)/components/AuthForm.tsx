5a315a6b03b4286833c9722298ddc89b52806d3c
kubonnt <kuba.sm.WTF@gmail.com>
2023-11-18 14:09:02 +0100

    Finished UI for auth

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,9 +1,12 @@
 "use client";
 
-import Input from "@/app/components/inputs/input";
-import Button from "@/app/components/button";
 import { useCallback, useState } from "react";
 import { FieldValues, useForm, SubmitHandler } from "react-hook-form";
+import { BsGithub, BsGoogle } from "react-icons/bs";
+
+import Input from "@/app/components/inputs/input";
+import Button from "@/app/components/button";
+import AuthSocialButton from "./AuthSocialButton";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -28,6 +31,7 @@ const AuthForm = () => {
       name: "",
       email: "",
       password: "",
+      confirmPassword: "",
     },
   });
 
@@ -60,7 +64,7 @@ const AuthForm = () => {
     >
       <div
         className="
-          bg-whites
+          bg-white
           px-4
           py-8
           shadow
@@ -70,7 +74,13 @@ const AuthForm = () => {
       >
         <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
           {variant === "REGISTER" && (
-            <Input id="Name" label="Name" register={register} errors={errors} />
+            <Input
+              id="Name"
+              label="Name"
+              register={register}
+              errors={errors}
+              disabled={isLoading}
+            />
           )}
           <Input
             id="email"
@@ -78,6 +88,7 @@ const AuthForm = () => {
             type="email"
             register={register}
             errors={errors}
+            disabled={isLoading}
           />
           <Input
             id="password"
@@ -85,11 +96,100 @@ const AuthForm = () => {
             type="password"
             register={register}
             errors={errors}
+            disabled={isLoading}
           />
+          {variant === "REGISTER" && (
+            <Input
+              id="confirmPassword"
+              label="Confirm Password"
+              type="confirmPassword"
+              register={register}
+              errors={errors}
+              disabled={isLoading}
+            />
+          )}
+
           <div>
-            <Button>Test</Button>
+            <Button disabled={isLoading} fullWidth type="submit">
+              {variant === "LOGIN" ? "Sign in" : "Register"}
+            </Button>
           </div>
         </form>
+
+        <div className="mt-6">
+          <div className="relative">
+            <div
+              className="
+                absolute
+                inset-0
+                flex
+                items-center
+              "
+            >
+              <div
+                className="
+                w-full
+                border-t
+                border-gray-300
+                "
+              />
+            </div>
+            <div
+              className="
+                relative
+                flex
+                justify-center
+                text-sm
+              "
+            >
+              <span
+                className="
+                  bg-white
+                  px-2
+                  text-gray-500
+                "
+              >
+                Or continue with
+              </span>
+            </div>
+          </div>
+          <div
+            className="
+              mt-6
+              flex
+              gap-2
+            "
+          >
+            <AuthSocialButton
+              icon={BsGithub}
+              onClick={() => socialAction("github")}
+            />
+            <AuthSocialButton
+              icon={BsGoogle}
+              onClick={() => socialAction("google")}
+            />
+          </div>
+        </div>
+        <div
+          className="
+            flex
+            gap-2
+            justify-center
+            text-sm
+            mt-6
+            px-2
+            text-gray-500
+          "
+        >
+          <div>
+            {variant === "LOGIN"
+              ? "New to the app?"
+              : "Already have an account?"}
+          </div>
+          <div onClick={toggleVariant} className="underline cursor-pointer">
+            {variant === "LOGIN" ? "Create an account" : "Login"}
+          </div>
+        </div>
       </div>
     </div>
   );
